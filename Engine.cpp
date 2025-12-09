bc986b62da838c094ff5c235e8a959c1e1bc9620
Hubi_Core <hbstojek@wp.pl>
2025-12-09 11:29:27 +0100

    zaczęty Engine

--- Engine.cpp
+++ Engine.cpp
@@ -1,3 +1,8 @@
 //
 // Created by Hubi_Core on 12/9/25.
 //
+#include "Engine.cuh"
+
+void Engine::Close() {
+    GLFWwindow* window = glfwCreateWindow(800, 600, "GLFW Test", nullptr, nullptr);
+}
--- Engine.cuh
+++ Engine.cuh
@@ -1,12 +1,25 @@
 //
 // Created by Hubi_Core on 12/9/25.
 //
-
+#include <GLFW/glfw3.h>
+#include <iostream>
 #ifndef ENGINE_CUH
 #define ENGINE_CUH
 
 class Engine {
     int resX, reY, fps;
+    public:
+    Engine(int resX, int resY, int fps) {
+        this->resX = resX;
+        this->reY = resY;
+        this->fps = fps;
+        GLFWwindow* window = glfwCreateWindow(resX, resY, "Silnik 3d", nullptr, nullptr);
+    };
+    void Close();
+
+
+
+
 
 };
 #endif //ENGINE_CUH
