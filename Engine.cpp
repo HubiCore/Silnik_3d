7687d91120e4c088688c2a3cac8ea1978fc2f8b9
Hubi_Core <hbstojek@wp.pl>
2025-12-09 11:04:30 +0100

    bruh

---
+++ Engine.cpp
@@ -0,0 +1,3 @@
+//
+// Created by Hubi_Core on 12/9/25.
+//
---
+++ Engine.cuh
@@ -0,0 +1,12 @@
+//
+// Created by Hubi_Core on 12/9/25.
+//
+
+#ifndef ENGINE_CUH
+#define ENGINE_CUH
+
+class Engine {
+    int resX, reY, fps;
+
+};
+#endif //ENGINE_CUH
