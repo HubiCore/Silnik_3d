3512a296180211e33c37947647c7d8954c9afec3
ASUS ROG <andzrejraumiagi@wp.pl>
2026-01-13 10:22:28 +0100

    powiekszenie i pomniejszenie sześciany pod KP_3, KP_9

--- main.cpp
+++ main.cpp
@@ -398,14 +398,14 @@ void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods
         }
     }
 
-    if (key == GLFW_KEY_EQUAL && action == GLFW_PRESS) {
+    if (key == GLFW_KEY_EQUAL && action == GLFW_PRESS || key == GLFW_KEY_KP_9 && action == GLFW_PRESS) {
         if (rotatingCube) {
             rotatingCube->scale(glm::vec3(1.1f));
             std::cout << "Powiekszono szescian" << std::endl;
         }
     }
 
-    if (key == GLFW_KEY_MINUS && action == GLFW_PRESS) {
+    if (key == GLFW_KEY_MINUS && action == GLFW_PRESS || key == GLFW_KEY_KP_3 && action == GLFW_PRESS) {
         if (rotatingCube) {
             rotatingCube->scale(glm::vec3(0.9f));
             std::cout << "Pomniejszono szescian" << std::endl;
